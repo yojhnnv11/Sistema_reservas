3020ad8251546e0f12129778c71a0a08696199c7
Yojhann <yojhann.v11@gmail.com>
2025-12-03 16:11:17 -0500

    Migracion al nuevo repo

--- app/db.py
+++ app/db.py
@@ -2,9 +2,20 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from app.core.config import settings
 
-engine = create_engine(settings.DATABASE_URL, echo=False, future=True)
-SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False, future=True)
+engine = create_engine(
+    settings.DATABASE_URL,
+    echo=False,
+    future=True
+)
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine,
+    future=True
+)
+
 Base = declarative_base()
+
 
 def get_db():
     db = SessionLocal()
--- app/main.py
+++ app/main.py
@@ -2,17 +2,18 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.db import Base, engine
 from app.routers import usuarios, canchas, reservas
-from app.models import models as models_module
+from app.models import models as models_module  # Importa todos los modelos
 
-# crea tablas si no existen
+# Crear automáticamente las tablas en PostgreSQL
 Base.metadata.create_all(bind=engine)
 
-app = FastAPI(title="Sistema Reservas API (MySQL)")
 
-# permitir el frontend local (ajusta origin si sirves desde XAMPP)
+app = FastAPI(title="Sistema Reservas API (PostgreSQL)")
+
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["*"],  # Permitir todos los orígenes
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -24,4 +25,4 @@ app.include_router(reservas.router)
 
 @app.get("/")
 def root():
-    return {"msg":"API funcionando"}
+    return {"msg": "API funcionando correctamente en Render"}
